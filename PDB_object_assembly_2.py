ac8c8617eddc7f13d6ba722c5bbd95420a368a5d
Will Wood <46889082+WhjWood@users.noreply.github.com>
2019-05-23 14:27:46 +0100

    Add files via upload
    2 layers with render

--- PDB_object_assembly_2.py
+++ PDB_object_assembly_2.py
@@ -6,7 +6,18 @@ import csv
 Population1_path = r"D:\Thylakoid_model\RANDOM_19_05_19\Data\POPULATION1_11000000"
 Population2_path = r"D:\Thylakoid_model\RANDOM_19_05_19\Data\POPULATION2_11000000"
 
+bpy.context.scene.layers[2] = True
+bpy.ops.mesh.primitive_cylinder_add(radius=230, depth=3, view_align=False, enter_editmode=False, location=(0, 0, 0), layers=(True, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False))
+bpy.context.scene.layers[0] = True
 
+if(len(bpy.data.cameras) == 1):
+    obj = bpy.data.objects['Camera'] # bpy.types.Camera
+    obj.location.x = 0.0
+    obj.location.y = -700.0
+    obj.location.z = 500.0
+    obj.rotation_euler[0] = 50.0
+    obj.rotation_euler[1] = 0.0
+    obj.rotation_euler[2]  = 5
 
 POPULATION1 = []
 with open(Population1_path, 'r') as f1:
@@ -22,13 +33,14 @@ with open(Population1_path, 'r') as f1:
 POPULATION2 = []
 with open(Population2_path, 'r') as f2:
     POP2 = csv.reader(f2)
-    for row in POP1:
+    for row in POP2:
         x = row[1]
         y = row[2]
         theta = row[3]
         ptype = row[4]
         POPULATION2.append([x,y,theta,ptype])
     
+bpy.context.scene.layers[0] = True
     
 for row in POPULATION1[1:]:
     x = float(row[0])
@@ -38,6 +50,8 @@ for row in POPULATION1[1:]:
     z_offset = 0
     scale = 0.1
     theta_offset = 0 # math.pi/4.0
+    
+    ATP_offset = 10
 
     if ptype == "C2S2M2":
         bpy.ops.import_scene.obj(filepath=r"F:\programs\Blender_scripting\C2S2M2_mod.obj")
@@ -66,7 +80,7 @@ for row in POPULATION1[1:]:
         bpy.context.selected_objects[0].location = (x,y,z_offset)
         bpy.context.selected_objects[0].rotation_euler[2] = theta%(2*math.pi)+theta_offset  
         bpy.context.selected_objects[0].active_material.diffuse_color = (0.8, 0, 0)
-    
+
     elif ptype == "B6F":
         bpy.ops.import_scene.obj(filepath=r"F:\programs\Blender_scripting\B6F_mod.obj")
         bpy.ops.transform.resize(value=(scale,scale,scale), constraint_axis=(False, False, False), constraint_orientation='GLOBAL', mirror=False, proportional='DISABLED', proportional_edit_falloff='SMOOTH', proportional_size=1)
@@ -77,27 +91,33 @@ for row in POPULATION1[1:]:
     elif ptype == "ATP":
         bpy.ops.import_scene.obj(filepath=r"F:\programs\Blender_scripting\ATP_mod.obj")
         bpy.ops.transform.resize(value=(scale,scale,scale), constraint_axis=(False, False, False), constraint_orientation='GLOBAL', mirror=False, proportional='DISABLED', proportional_edit_falloff='SMOOTH', proportional_size=1)
-        bpy.context.selected_objects[0].location = (x,y,z_offset)
+        bpy.context.selected_objects[0].location = (x,y,z_offset+ATP_offset )
         bpy.context.selected_objects[0].rotation_euler[2] = theta%(2*math.pi)+theta_offset 
     
     elif ptype == "PSII_mono":
         bpy.ops.import_scene.obj(filepath=r"F:\programs\Blender_scripting\PSIImono_mod.obj")
         bpy.ops.transform.resize(value=(scale,scale,scale), constraint_axis=(False, False, False), constraint_orientation='GLOBAL', mirror=False, proportional='DISABLED', proportional_edit_falloff='SMOOTH', proportional_size=1)
         bpy.context.selected_objects[0].location = (x,y,z_offset)
-        bpy.context.selected_objects[0].rotation_euler[2] = theta%(2*math.pi)+theta_offset 
+    bpy.context.selected_objects[0].rotation_euler[2] = theta%(2*math.pi)+theta_offset 
 
 
 
 ### Population 2 (upside down) ###
+
+
+bpy.context.scene.layers[2] = True
+bpy.ops.mesh.primitive_cylinder_add(radius=230, depth=3, view_align=False, enter_editmode=False, location=(0, 0, -20.0), layers=(True, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False))
+bpy.context.scene.layers[1] = True
 
 for row in POPULATION2[1:]:
     x = float(row[0])
     y = float(row[1])
     theta = float(row[2])
     ptype = row[3]    
-    z_offset = -10.0
+    z_offset = -20.0
     scale = 0.1
     theta_offset = 0 # math.pi/4.0
+    ATP_offset = 10
 
     if ptype == "C2S2M2":
         bpy.ops.import_scene.obj(filepath=r"F:\programs\Blender_scripting\C2S2M2_mod.obj")
@@ -142,7 +162,7 @@ for row in POPULATION2[1:]:
     elif ptype == "ATP":
         bpy.ops.import_scene.obj(filepath=r"F:\programs\Blender_scripting\ATP_mod.obj")
         bpy.ops.transform.resize(value=(scale,scale,scale), constraint_axis=(False, False, False), constraint_orientation='GLOBAL', mirror=False, proportional='DISABLED', proportional_edit_falloff='SMOOTH', proportional_size=1)
-        bpy.context.selected_objects[0].location = (x,y,z_offset)
+        bpy.context.selected_objects[0].location = (x,y,z_offset-ATP_offset)
         bpy.context.selected_objects[0].rotation_euler[1] = math.pi
         bpy.context.selected_objects[0].rotation_euler[2] = theta%(2*math.pi)+theta_offset 
     
@@ -153,3 +173,5 @@ for row in POPULATION2[1:]:
         bpy.context.selected_objects[0].rotation_euler[1] = math.pi
         bpy.context.selected_objects[0].rotation_euler[2] = theta%(2*math.pi)+theta_offset 
 
+
+bpy.ops.render.render()
